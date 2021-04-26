0387714bb6773416926903fb17fc3b3697e6d2af
Aritra Kar <aritrakar2002@gmail.com>
2021-04-26 10:50:52 +0530

    Update player.py

--- Blackjack/player.py
+++ Blackjack/player.py
@@ -1,28 +1,33 @@
 import card
 
-
 class Player:
     def __init__(self, cards):
         self.cards = cards
 
+    # numCards() returns the number of cards in the Player's hand
     def numCards(self):
         return len(self.cards)
 
+    # getCard(index) returns the card at index 'index' of the player's hand
     def getCard(self, index):
         return self.cards[index]
 
+    # printCard(index) prints the card at index 'index' of the player's hand
     def printCard(self, index):
         self.cards[index].printCard()
 
+    # printAllCards() prints all the cards in player's hand
     def printAllCards(self):
-        num = len(self.cards)  # self.numCards()
+        num = len(self.cards)
         for c in range(num):
             self.cards[c].printCard()
 
+    # addCard(card) adds Card 'card' to player's hand
     def addCard(self, card):
         self.cards += [card]
         print("Added: ")
         card.printCard()
 
+    # removeCard(index) removes Card at 'index' from player's hand
     def removeCard(self, index):
         return self.cards.pop(index)
